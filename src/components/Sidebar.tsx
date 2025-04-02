9e8ced8d124b70707b984991707add6e7477feb1
Camilo Code <camilorodriguezco7@gmail.com>
2025-04-01 23:12:11 -0500

    Feat: Se creo la funcion de cerra sesión

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,12 +1,19 @@
 import { Link } from "react-router-dom";
 import Logo from "./Logo";
 import { User } from "../types";
+import { useQueryClient } from "@tanstack/react-query";
 
 type SideMenuProps = {
   user: User
 }
 
 export default function Sidebar({user}: SideMenuProps) {
+  const queryClient = useQueryClient()
+
+  const logout = () => {
+    localStorage.removeItem('AUTH_TOKEN')
+    queryClient.invalidateQueries({queryKey: ['user']})
+  }
   return (
     <aside className="md:w-1/4 lg:w-1/5 xl:w-1/6 px-5 md:py-10 py-5 bg-[#4B62D3] text-left ">
       <Logo/>
@@ -17,6 +24,11 @@ export default function Sidebar({user}: SideMenuProps) {
             <Link className="block mb-5 ml-2 text-sm" to="/projects/create">Crear Projectos</Link>
             <Link className="block mb-5" to="/users">Usuarios</Link>
             <Link className="block mb-5 ml-2 text-sm" to="/users/register">Crear Usuarios</Link>
+            <button 
+              className="block p-2 rounded-2xl bg-red-700 text-white hover:bg-red-600 cursor-pointer"
+              onClick={logout}
+            >Cerrar Sesión</button>
+
         </div>
     </aside>
   )
