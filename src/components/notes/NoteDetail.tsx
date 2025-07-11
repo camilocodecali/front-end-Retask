b9a236a15cde7f609c810798e6bce1d5b4b40d0f
Camilo Code <camilorodriguezco7@gmail.com>
2025-07-10 23:32:42 -0500

    Feat: Se elimina las notas si eres el creador

--- src/components/notes/NoteDetail.tsx
+++ src/components/notes/NoteDetail.tsx
@@ -3,6 +3,10 @@ import { useAuth } from "@/hooks/useAuth"
 import { Note } from "@/types/index"
 import { useMemo } from "react"
 import Spinner from "../Spinner"
+import { useMutation, useQueryClient } from "@tanstack/react-query"
+import { deleteNote } from "@/api/NoteAPI"
+import { toast } from "react-toastify"
+import { useLocation, useParams } from "react-router-dom"
 
 type NoteDetailProps = {
     note: Note
@@ -11,6 +15,23 @@ type NoteDetailProps = {
 export default function NoteDetail({note}: NoteDetailProps) {
     const {data, isLoading} = useAuth()
     const canDelete = useMemo(()=> data?._id === note.createdBy._id , [data])
+    const params = useParams()
+
+      const location = useLocation();
+      const queryParams = new URLSearchParams(location.search);
+      const queryClient = useQueryClient()
+
+      const projectId = params.projectId!
+      const taskId = queryParams.get("viewTask")!;
+
+    const {mutate} = useMutation({
+        mutationFn: deleteNote,
+        onError: (error) => toast.error(error.message),
+        onSuccess: (data) => {
+            queryClient.invalidateQueries({queryKey:['task', taskId]})
+            toast.success(data)
+        }
+    })
 
     if (isLoading) return <Spinner/>
   return (
@@ -24,7 +45,10 @@ export default function NoteDetail({note}: NoteDetailProps) {
         </p>
       </div>
       {canDelete && (
-        <button className="bg-red-400 hover:bg-red-500 p-2 text-xs text-white font-bold cursor-pointer transition-colors rounded-xl">Eliminar</button>
+        <button
+         onClick={()=>mutate({projectId, taskId, noteId: note._id})}
+         className="bg-red-400 hover:bg-red-500 p-2 text-xs text-white font-bold cursor-pointer transition-colors rounded-xl"
+        >Eliminar</button>
       )}
     </div>
   )
